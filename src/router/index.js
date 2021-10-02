96d9600dba34f4a0e7ec06338deb510b19693886
Dmitry <dp_140482@mail.ru>
2021-10-02 16:20:07 +0300

    ДЗ 5 с багами

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+const router = new Router({
+    mode: 'history',
+    routes: [{
+        path: '/add/payment/:category',
+        name: 'addpayment'
+    }, {
+        path: '/add/payment',
+        name: 'addvalue'
+    }]
+})
+
+/*
+router.afterEach((to, from) => {
+    to.
+})
+*/
+
+export default router;
